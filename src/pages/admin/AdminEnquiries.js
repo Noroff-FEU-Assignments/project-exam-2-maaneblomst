1ef0d3347555bad427aa9865c660e1e76818b9d0
maaneblomst <maaneblomst@users.noreply.github.com>
2021-09-17 01:42:21 +0200

    Update AdminEnquiries.js

--- src/pages/admin/AdminEnquiries.js
+++ src/pages/admin/AdminEnquiries.js
@@ -1,12 +1,15 @@
 import { EnquiriesList } from "../../components/enquiries/EnquiriesList";
 import Heading from "../../components/common/Heading";
 import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function AdminEnquiries() {
   return (
     <Container>
       <Heading size="1" content="Enquiries" />
-      <EnquiriesList />
+      <ListGroup>
+        <EnquiriesList />
+      </ListGroup>
     </Container>
   );
 }
